5f392c59f9646f0748730aa00fac150ef656aa15
andole <44316441+andoleal@users.noreply.github.com>
2023-12-29 14:36:37 +0000

    Auto versioning

--- dist/make.bat
+++ dist/make.bat
@@ -1,5 +1,7 @@
 xcopy ..\bin\Release\net6.0\FewerPasscodeNotes.dll FewerPasscodeNotes /y
 xcopy ..\README.md FewerPasscodeNotes /y
 
+powershell -Command "$data = Get-Content 'FewerPasscodeNotes/manifest.json' -raw | ConvertFrom-Json;$major,$minor,$build = $data.version_number.Split('.');$build = 1 + $build;$data.version_number = $major,$minor,$build -join '.';$data | ConvertTo-Json -depth 32| set-content 'FewerPasscodeNotes/manifest.json'"
+
 del FewerPasscodeNotes.zip
 powershell Compress-Archive FewerPasscodeNotes FewerPasscodeNotes.zip
